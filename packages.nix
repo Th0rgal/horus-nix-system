0b82d8640ca32f4b3214d77545698d965380bf82
Th0rgal <thomas.marchand@tuta.io>
2021-01-27 11:47:30 +0100

    remove vscode

--- packages.nix
+++ packages.nix
@@ -10,71 +10,11 @@
   services.usbmuxd.enable = true;
 
   imports = [
-    ./vscode.nix
     ./modules/howdy.nix
     ./modules/ir_toggle.nix
   ];
 
-  vscode.user = "thomas";
-  vscode.homeDir = "/home/thomas";
-  vscode.extensions = (with pkgs.vscode-extensions; [
-    bbenoist.Nix ms-python.python ms-vscode.cpptools
-  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "latex-workshop";
-        publisher = "James-Yu";
-        version = "8.15.0";
-        sha256 = "0v4pq3l6g4dr1qvnmgsw148061lngwmk3zm12q0kggx85blki12d";
-      }
-      {
-        name = "better-toml";
-        publisher = "bungcip";
-        version = "0.3.2";
-        sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
-      }
-      {
-        name = "docs-yaml";
-        publisher = "docsmsft";
-        version = "0.2.6";
-        sha256 = "0fsvq77sdp7wln7xfc1yy3x30pdvk2rgy056i3vlrgvlk8gbzi2n";
-      }
-      {
-        name = "markdown-preview-enhanced";
-        publisher = "shd101wyy";
-        version = "0.5.16";
-        sha256 = "0w5w2np8fkxpknq76yv8id305rxd8a1p84p9k0jwwwlrsyrz31q8";
-      }
-      {
-        name = "vsc-material-theme-icons";
-        publisher = "Equinusocio";
-        version = "1.2.2";
-        sha256 = "06xfv8ggli88zg1hyrd7m494fl6bz4fspbxy626nsswq4f26msms";
-      }
-      {
-        name = "cmake-tools";
-        publisher = "ms-vscode";
-        version = "1.5.3";
-        sha256 = "1y2s8rsc94ywbr23x9lhz0idp0d2lbv2vck636blvxsgxmcmmx8d";
-      }
-      {
-        name = "lean";
-        publisher = "jroesch";
-        version = "0.16.22";
-        sha256 = "08fj7h6dw56s40l9fkn4xz1valxpwhc4n8vw7d44264cabcsmkrw";
-      }
-      {
-        name = "nix-env-selector";
-        publisher = "arrterian";
-        version = "0.1.2";
-        sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
-      }
-    ];
-
   nixpkgs = {
-    latestPackages = [
-      "vscode"
-      "vscode-extensions"
-    ];
 
     overlays = [
       (self: super: {
